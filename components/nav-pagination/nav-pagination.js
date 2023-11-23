f35b0bffcaf0698773b1f2ef530a39d67a5d2350
Burak Y <buriii@hotmail.de>
2023-11-23 21:34:41 +0100

    pagination.js changes

--- components/nav-pagination/nav-pagination.js
+++ components/nav-pagination/nav-pagination.js
@@ -1,28 +1,28 @@
 export function createPagination() {
-const mainContent = document.getElementById("main-content");
+  const mainContent = document.getElementById("main-content");
 
-const navigation = document.createElement("nav");
-navigation.classList.add("navigation");
-navigation.setAttribute("data-js", "navigation");
+  const navigation = document.createElement("nav");
+  navigation.classList.add("navigation");
+  navigation.setAttribute("data-js", "navigation");
 
-const prevButton = document.createElement("button");
-prevButton.classList.add("button", "button--prev");
-prevButton.setAttribute("data-js", "button-prev");
-prevButton.textContent = "previous";
+  const prevButton = document.createElement("button");
+  prevButton.classList.add("button", "button--prev");
+  prevButton.setAttribute("data-js", "button-prev");
+  prevButton.textContent = "previous";
 
-const paginationSpan = document.createElement("span");
-paginationSpan.classList.add("navigation__pagination");
-paginationSpan.setAttribute("data-js", "pagination");
-paginationSpan.textContent = "1 / 42"; // You can update this dynamically based on your logic
+  const paginationSpan = document.createElement("span");
+  paginationSpan.classList.add("navigation__pagination");
+  paginationSpan.setAttribute("data-js", "pagination");
+  paginationSpan.textContent = "1 / 42";
 
-const nextButton = document.createElement("button");
-nextButton.classList.add("button", "button--next");
-nextButton.setAttribute("data-js", "button-next");
-nextButton.textContent = "next";
+  const nextButton = document.createElement("button");
+  nextButton.classList.add("button", "button--next");
+  nextButton.setAttribute("data-js", "button-next");
+  nextButton.textContent = "next";
 
-navigation.appendChild(prevButton);
-navigation.appendChild(paginationSpan);
-navigation.appendChild(nextButton);
+  navigation.appendChild(prevButton);
+  navigation.appendChild(paginationSpan);
+  navigation.appendChild(nextButton);
 
-mainContent.appendChild(navigation);
+  mainContent.appendChild(navigation);
 }
